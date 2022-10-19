d30bc09c16af5916a2d925071e85379871082285
Nicholas <joshcooldev@gmail.com>
2022-10-19 12:32:56 -0700

    websocket update 3

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,10 +38,16 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-                'encrypted' => true,
+                // 'encrypted' => true,
+                // 'host' => '127.0.0.1',
                 'host' => 'api.syndicatesms.com',
-                'port' => 6001,
-                'scheme' => 'https'
+                // 'port' => 6001,
+                'scheme' => 'https',
+                // 'scheme' => 'http',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
         ],
 
