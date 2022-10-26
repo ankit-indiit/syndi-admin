13ff3c2955ef78ad4e8c442790ef0bea78539e36
Nicholas <joshcooldev@gmail.com>
2022-10-26 10:44:33 -0700

    22-10-26 outbox - get schedule list

--- app/Http/Controllers/API/Message/OutboxController.php
+++ app/Http/Controllers/API/Message/OutboxController.php
@@ -73,7 +73,22 @@ class OutboxController extends Controller
      */
     public function show($id)
     {
-        //
+        $user_phone = Auth::user()->phone;
+        $schedules = Msg::with(['img' => function ($query) {
+                            $query->select('msg_id', 'img_url');
+                        }])
+                        ->where(function ($query) use ($user_phone) {
+                            $query->where('sender_phone', '=', $user_phone);
+                                    // ->orWhere('receiver_phone', '=', $user_phone);
+                        })
+                        ->where('schedule_at', '!=', null)
+                        ->orderBy('created_at', 'DESC')
+                        ->get()
+                        ->groupBy(['message', 'schedule_at']);
+        
+        $schedule_arr = $this->getScheduleArray($schedules);
+        return response()->json($schedule_arr);
+        // return response()->json($schedules);
     }
 
     /**
@@ -116,39 +131,34 @@ class OutboxController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    protected function getLastMessages($messages)
+    protected function getScheduleArray($schedules)
     {
-        $messages_array = array();
-        foreach ($messages as $key => $message_arr)
+        $schedule_arr = array();
+        foreach ($schedules as $message => $schedule_q)
         {
-            $sort_array = $message_arr->toArray();
-            usort($sort_array, function($first, $second) {
-                return $first['created_at'] < $second['created_at'];
-            });
             $sub_arr = [];
-            $sub_arr['sender_phone'] = '';
-            $sub_arr['sender_name'] = '';
-            $sub_arr['receiver_phone'] = '';
-            $sub_arr['receiver_name'] = '';
-            $sub_arr['message'] = '';
-            $sub_arr['created_at'] = '';
-
-            if (!is_null($sort_array[0]))
+            $sub_arr['message'] = $message;
+            $sub_arr['sender_phone'] = Auth::user()->phone;
+            
+            foreach ($schedule_q as $schedule_at => $phone_q)
             {
-                $img_arr = [];
-                foreach ($sort_array[0]['img'] as $key => $img) {
-                    array_push($img_arr, $img['img_url']);
+                $sub_arr['receiver_phones'] = [];
+                $sub_arr['schedule_at'] = $schedule_at;
+                
+                foreach ($phone_q as $key => $value)
+                {
+                    array_push($sub_arr['receiver_phones'], $value->receiver_phone);
+                    
+                    $sub_arr['imageUrls'] = [];
+                    foreach ($value->img as $img_key => $image)
+                    {
+                        array_push($sub_arr['imageUrls'], $image->img_url);
+                    }
                 }
-                $sub_arr['sender_phone'] = $sort_array[0]['sender_phone'];
-                $sub_arr['sender_name'] = $sort_array[0]['sender_name'];
-                $sub_arr['receiver_phone'] = $sort_array[0]['receiver_phone'];
-                $sub_arr['receiver_name'] = $sort_array[0]['receiver_name'];
-                $sub_arr['message'] = $sort_array[0]['message'];
-                $sub_arr['created_at'] = $sort_array[0]['created_at'];
-                $sub_arr['imgs'] = $img_arr;
             }
-            array_push($messages_array, $sub_arr);
+
+            array_push($schedule_arr, $sub_arr);
         }
-        return $messages_array;
+        return $schedule_arr;
     }
 }
